4c26aa40f0740606dd4040ddfe31a470bea2702f
jakekrol <jdkrol351@gmail.com>
2025-01-09 10:24:55 -0700

    preview h5 cxg

--- preview_h5ad.py
+++ preview_h5ad.py
@@ -6,25 +6,34 @@ import numpy as np
 
 # Get the .h5ad file from command line arguments
 h5ad_file = sys.argv[1]
+type = sys.argv[2]
+explore = sys.argv[3]
 
 # Load the .h5ad file
 adata = ad.read_h5ad(h5ad_file)
 
 # Extract the data, index, and columns
-matrix_data = adata.X
-index = adata.obs['barcodes']
-columns = adata.var['genes']
+if type == 'encode':
+    matrix_data = adata.X
+    index = adata.obs['barcodes']
+    columns = adata.var['genes']
 
-# Check if matrix_data is a sparse matrix and convert to dense if necessary
-if not isinstance(matrix_data, np.ndarray):
-    matrix_data = matrix_data.toarray()
+    # Check if matrix_data is a sparse matrix and convert to dense if necessary
+    if not isinstance(matrix_data, np.ndarray):
+        matrix_data = matrix_data.toarray()
 
-# Create the DataFrame
-restored_df = pd.DataFrame(matrix_data, index=index, columns=columns)
+    # Create the DataFrame
+    df = pd.DataFrame(matrix_data, index=index, columns=columns)
+if type == 'cxg':
+    df = adata.to_df()
+    df.columns = adata.var['gene_name']
 
 # Print the head and shape of the DataFrame
 print("Head of the DataFrame:")
-print(restored_df.head())
+print(df.head())
 
 print("\nShape of the DataFrame:")
-print(restored_df.shape)
+print(df.shape)
+
+if explore:
+    breakpoint()
